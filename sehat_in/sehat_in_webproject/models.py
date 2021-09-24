32adf91f79a87ad55271c8e16dadbb06590267eb
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2021-09-24 20:09:13 +0700

    profile role update

--- sehat_in/sehat_in_webproject/models.py
+++ sehat_in/sehat_in_webproject/models.py
@@ -68,8 +68,6 @@ class UserProfile(models.Model):
     bio = models.TextField(max_length=500, blank=True, null=True)
     location = models.CharField(max_length=250, blank=True, null=True)
     role = models.TextField(max_length=100, blank=True, null=True)
-    # cek post apa aja, if post.user.id == user.id
-    # cek notif, if notif.user.id == user.id
 
 class Report(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='reporter') # The one that reports
