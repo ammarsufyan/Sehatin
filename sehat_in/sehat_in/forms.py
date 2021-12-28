b48fea9313c170ec41e01930e835a9456b5fca18
Ammar Sufyan <ammarsufyan78@gmail.com>
2021-12-28 10:00:15 +0700

    Update forms.py

--- sehat_in/sehat_in/forms.py
+++ sehat_in/sehat_in/forms.py
@@ -1,7 +1,5 @@
 from django import forms
-from django.contrib.auth import (
-    get_user_model, password_validation,
-)
+from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 import functools
@@ -17,49 +15,6 @@ from django.utils.module_loading import import_string
 from django.utils.translation import gettext as __, ngettext
 
 UserModel = get_user_model()
-
-class MyPasswordForm(forms.Form):
-    """
-    A form that lets a user change set their password without entering the old
-    password
-    """
-    error_messages = {
-        'password_mismatch': _('Kata sandi tidak cocok.'),
-    }
-    new_password1 = forms.CharField(
-        label=_("Kata Sandi Baru"),
-        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
-        strip=False,
-        help_text=password_validation.password_validators_help_text_html(),
-    )
-    new_password2 = forms.CharField(
-        label=_("Konfirmasi Kata Sandi Baru"),
-        strip=False,
-        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
-    )
-
-    def __init__(self, user, *args, **kwargs):
-        self.user = user
-        super().__init__(*args, **kwargs)
-
-    def clean_new_password2(self):
-        password1 = self.cleaned_data.get('new_password1')
-        password2 = self.cleaned_data.get('new_password2')
-        if password1 and password2:
-            if password1 != password2:
-                raise ValidationError(
-                    self.error_messages['password_mismatch'],
-                    code='password_mismatch',
-                )
-        password_validation.validate_password(password2, self.user)
-        return password2
-
-    def save(self, commit=True):
-        password = self.cleaned_data["new_password1"]
-        self.user.set_password(password)
-        if commit:
-            self.user.save()
-        return self.user
 
 @functools.lru_cache(maxsize=None)
 def get_default_password_validators():
@@ -233,3 +188,46 @@ class UpperLowerCasePasswordValidator:
 
     def get_help_text(self):
         return __('Kata sandi Anda harus memiliki huruf kapital dan huruf kecil')
+
+class MyPasswordForm(forms.Form):
+    """
+    A form that lets a user change set their password without entering the old
+    password
+    """
+    error_messages = {
+        'password_mismatch': _('Kata sandi tidak cocok.'),
+    }
+    new_password1 = forms.CharField(
+        label=_("Kata Sandi Baru"),
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+        strip=False,
+        help_text=_password_validators_help_text_html(),
+    )
+    new_password2 = forms.CharField(
+        label=_("Konfirmasi Kata Sandi Baru"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+    )
+
+    def __init__(self, user, *args, **kwargs):
+        self.user = user
+        super().__init__(*args, **kwargs)
+
+    def clean_new_password2(self):
+        password1 = self.cleaned_data.get('new_password1')
+        password2 = self.cleaned_data.get('new_password2')
+        if password1 and password2:
+            if password1 != password2:
+                raise ValidationError(
+                    self.error_messages['password_mismatch'],
+                    code='password_mismatch',
+                )
+        validate_password(password2, self.user)
+        return password2
+
+    def save(self, commit=True):
+        password = self.cleaned_data["new_password1"]
+        self.user.set_password(password)
+        if commit:
+            self.user.save()
+        return self.user
